fb14a0a51ffbc25a1390d4960dea907f398aeb57
yuinaibur <rubiasoft1015@gmail.com>
2022-10-09 20:38:09 +0900

    ゼミでクイズを作りました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("button")
+
+button.onclick = function() {
+  alert("クリックしたね")
+}
+
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function(e) {
+  console.log(e.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,7 +1,11 @@
-const genkiFunction = function(number) {
+const hattenn1 = function(number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(n + "!!!!!!!")
+    if (n % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 3 === 0) {
+      console.log("Fizz")
+    } else if (n % 5 === 0) {
+      console.log("Buzz")
     } else {
       console.log(n)
     }

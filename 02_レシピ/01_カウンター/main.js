ad2add2cfb818bf6d3ad59d04a0411804b51ab09
yuinaibur <rubiasoft1015@gmail.com>
2022-10-10 22:37:52 +0900

    [カウンター]を学習しました。

---
+++ 02_レシピ/01_カウンター/main.js
@@ -0,0 +1,10 @@
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
+
+let count = 0
+
+plusButton.onclick = function() {
+  count += 1
+
+  display.textContent = count
+}
